a712b4cc6091803c3f94a9a5998dbaeb2ac1d32a
Shcuryk Yevhenii <61512801+Modizan@users.noreply.github.com>
2021-05-01 20:23:01 +0300

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,3 @@
-// const http = require('http');
-// const fs = require('fs');
-//
-// const hostname = "localhost";
-// const port = 8080;
-//
-// const server = http.createServer((req, res) => {
-//     fs.readFile('./src/html/index.html', function (err, data){
-//         res.writeHead(200, {'Content-Type': 'text/html'});
-//         res.write(data);
-//         return res.end();
-//     });
-// })
-//
-// server.listen(port, hostname, () => {
-//     console.log(`Server is running at http://${hostname}:${port}/`);
-// });
 const scrapper = require('./scrapper');
 const fs = require('fs');
 
