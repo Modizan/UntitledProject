d252f80475495efb34142104ca7b52a2ceac60cc
Modizan <mrsilkxman@gmail.com>
2021-05-18 21:01:29 +0300

    Scrapper 1.2.3(NoDocFin)

--- src/js/app.js
+++ src/js/app.js
@@ -1,30 +1,61 @@
-// const http = require('http');
-// const fs = require('fs');
-//
-// const hostname = "localhost";
-// const port = 8080;
-//
-// const server = http.createServer((req, res) => {
-//     fs.readFile('./src/html/index.html', function (err, data){
-//         res.writeHead(200, {'Content-Type': 'text/html'});
-//         res.write(data);
-//         return res.end();
-//     });
-// })
-//
-// server.listen(port, hostname, () => {
-//     console.log(`Server is running at http://${hostname}:${port}/`);
-// });
 const scrapper = require('./scrapper');
 const fs = require('fs');
+const chalk = require('chalk')
+const cliProgress = require('cli-progress')
 
-(async () => {
-    await scrapper.initialize('Necrontyr');
+let config = require('../config/config.json')
 
-    let results = await scrapper.getResults(10);
+let Queue = require('queue-promise')
 
-    fs.writeFileSync(
-        require.resolve('../collectedData.json'),
-        JSON.stringify(results, null, " ")
-    )
-}) ();
+async function processing () {
+
+
+    let data = []
+
+
+    const queue = new Queue({
+        concurrent: 1,
+        interval: 5
+    });
+
+
+    const progressBar = new cliProgress.SingleBar({
+        format: '{bar} {percentage}% | {value}/{total} | elapsed: {duration_formatted} | estimated: {eta_formatted} Current Source: ' + chalk.blue('{source}'),
+        hideCursor: true
+    })
+
+
+    progressBar.start(config.length, 0)
+
+
+    queue.on("resolve", () => {
+        progressBar.update(data.length, {source: channel.source})
+    })
+
+
+    queue.on('end', () => {
+        progressBar.stop()
+
+        fs.writeFileSync(
+            'src/config/collectedData.json',
+            JSON.stringify(data, null, " ")
+        )
+
+        console.log("Finished")
+    })
+
+
+    config.forEach(channel => {
+        queue.enqueue(() => scrapper.initialize(channel["source"], 1).then( res => {
+            data.push({"Source": channel["source"], "Response": res})
+            progressBar.update(data.length, {source: channel.source})
+            return res
+        }))
+    })
+
+
+    queue.start()
+
+}
+
+processing()
