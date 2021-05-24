8fbf1818809da0c60c7fceef446925b5f91e9100
Shcuryk Yevhenii <61512801+Modizan@users.noreply.github.com>
2021-05-24 11:10:42 +0300

    Update scrapper.test.js

--- src/test/scrapper.test.js
+++ src/test/scrapper.test.js
@@ -1,7 +1,7 @@
 scrapper = require('../js/scrapper');
 let config = require('../config/config.json')
 
-test("", () => {
+test("If there is syntax problems in confsg.json, the test is failed", () => {
     expect(() => config.forEach(channel => {
         scrapper.initialize(channel, 1)
     })).not.toThrowError(SyntaxError);
